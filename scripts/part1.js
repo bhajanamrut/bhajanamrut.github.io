a7b800e7ab8379088e7ebe1051542da4701617f5
bhajanamrut <dattabhajanamrut@gmail.com>
2024-10-09 11:15:56 +0530

    Update part1.js

--- scripts/part1.js
+++ scripts/part1.js
@@ -1,5 +1,5 @@
 ﻿var p1 = [
-    { eng: 'pratham tula vandito ganapati ganesh', hin: 'प्रथम तुला वंदितो', vol: '1', pn: '3' },
+    { eng: 'pratham tula vandito ganapati ganesh', hin: 'प्रथम तुला वंदितो', vol: 'p1', pn: '2' },
  
 ];
 
