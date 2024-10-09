fa90e46ea4547900a0bc48cb81715431cdbfda00
bhajanamrut <dattabhajanamrut@gmail.com>
2024-10-09 10:48:17 +0530

    Create part1.js

---
+++ scripts/part1.js
@@ -0,0 +1,8 @@
+﻿var p1 = [
+    { eng: 'pratham tula vandito', hin: 'प्रथम तुला वंदितो', vol: '1', pn: '3' },
+ 
+];
+
+if (typeof module === 'object' && module.exports) {
+    module.exports = p1;
+}
