c2b2cb2ff04e761d66b5d195275d38570127a987
bhajanamrut <dattabhajanamrut@gmail.com>
2024-10-12 11:53:23 +0530

    Update part2.js

--- scripts/part2.js
+++ scripts/part2.js
@@ -6,7 +6,7 @@ var p2 = [
      { eng: 'devi kya shan shaan hai tulaja', hin: 'क्या शान है तुलजा भवानी की', dir: '2', id: '5' },
      { eng: 'veda houni nache anand', hin: 'वेडा होऊनी नाचे आनंद', dir: '2', id: '6' },
      { eng: 'savaliya giridhari lal khabar', hin: 'सांवलिया गिरिधारी लाल खबर लेना मेरी', dir: '2', id: '7' },
-     { eng: 'devi painjanacha nad naad', hin: 'पैंजनाचा नाद', dir: '2', id: '7' },
+     { eng: 'devi painjanacha nad naad', hin: 'पैंजनाचा नाद', dir: '2', id: '8' },
      { eng: 'devi dongaravari', hin: 'डोंगरावरी डोंगर ग', dir: '2', id: '9' },
 
 
