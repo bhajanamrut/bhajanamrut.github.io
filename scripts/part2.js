ae7dc63ff63fac2555bb76262b44126d9fa89275
bhajanamrut <dattabhajanamrut@gmail.com>
2024-10-12 11:46:42 +0530

    Update part2.js

--- scripts/part2.js
+++ scripts/part2.js
@@ -1,5 +1,15 @@
 var p2 = [
-     { eng: 'navaratra devo arati ashwin masi shukla', hin: 'नवरात्र देवी आरती', dir: '2', id: '1' },
+     { eng: 'navaratri devi arati ashwin masi shukla', hin: 'नवरात्र देवी आरती', dir: '2', id: '1' },
+     { eng: 'devi aai bhavani tuzya krupene', hin: 'आई भवानी तुझ्या कृपेने', dir: '2', id: '2' },
+     { eng: 'devi chalo bulava aya hai', hin: 'चलो बुलावा आया है', dir: '2', id: '3' },
+     { eng: 'gajanan shegaon nivasi maharaj mere', hin: 'शेगांव निवासी, महाराज मेरे', dir: '2', id: '4' }, 
+     { eng: 'devi kya shan shaan hai tulaja', hin: 'क्या शान है तुलजा भवानी की', dir: '2', id: '5' },
+     { eng: 'veda houni nache anand', hin: 'वेडा होऊनी नाचे आनंद', dir: '2', id: '6' },
+     { eng: 'savaliya giridhari lal khabar', hin: 'सांवलिया गिरिधारी लाल खबर लेना मेरी', dir: '2', id: '7' },
+     { eng: 'devi painjanacha nad naad', hin: 'पैंजनाचा नाद', dir: '2', id: '7' },
+     { eng: 'devi dongaravari', hin: 'डोंगरावरी डोंगर ग', dir: '2', id: '9' },
+
+
 ];
 
 if (typeof module === 'object' && module.exports) {
