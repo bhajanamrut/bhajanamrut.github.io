45faaf66ea28b3253f8c4bc3211102bf1c527829
nanadaas <32093456+nanadaas@users.noreply.github.com>
2025-03-16 19:41:42 +0530

    Update part2.js

--- scripts/part2.js
+++ scripts/part2.js
@@ -39,7 +39,7 @@ var p2 = [
      { eng: 'krushnatirichya vasanarya', hin: 'कृष्णातिरीच्या वसणाऱ्या', dir: '2', id: '38' },
      { eng: 'alikad ye mala palikad', hin: 'अलीकड ये, मला पलीकड ने', dir: '2', id: '39' },
      { eng: 'sanso saso ki mala', hin: 'साँसों की माला पे', dir: '2', id: '41' },
-     //{ eng: '', hin: '', dir: '2', id: '42' },
+     { eng: 'karuna tripadi', hin: 'करुणात्रिपदी', dir: '2', id: '42' },
      //{ eng: '', hin: '', dir: '2', id: '43' },
      //{ eng: '', hin: '', dir: '2', id: '44' },
      //{ eng: '', hin: '', dir: '2', id: '45' },
