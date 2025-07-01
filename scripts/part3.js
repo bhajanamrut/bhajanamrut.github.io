17e493e44775f89759a2b614cc9f92cf663ed4d7
shrinanabhajan <shrinanabhajan@gmail.com>
2025-07-01 08:43:45 +0530

    Added bhajans

--- scripts/part3.js
+++ scripts/part3.js
@@ -1,12 +1,11 @@
 var p3 = [
      { eng: 'Manik Ganraj payi man jad', hin: 'गणराजपायीं मन जड जड', dir: '3', id: '1' },
-     
-     //{ eng: '', hin: '', dir: '2', id: '2' },
-     //{ eng: '', hin: '', dir: '2', id: '3' },
-     //{ eng: '', hin: '', dir: '2', id: '4' },
-     //{ eng: '', hin: '', dir: '2', id: '5' },
-     //{ eng: '', hin: '', dir: '2', id: '6' },
-     //{ eng: '', hin: '', dir: '2', id: '7' },
+     { eng: 'Manik prabhuvin kon kunacha vali', hin: 'प्रभुविण कोण कुणाचा वाली', dir: '2', id: '2' },
+     { eng: 'Manik ig yenu pelali guraraya', hin: 'ईग येनु पेळलि गुरुराया', dir: '2', id: '3' },
+     { eng: 'Manik shri guru maza datta dayaghan', hin: 'श्री गुरु माझा दत्त दयाघन रे', dir: '2', id: '4' },
+     { eng: 'Manik guruji tore paiyape sees dharu', hin: 'गुरुजी तोरे पैयांपे सीस धरूं', dir: '2', id: '5' },
+     //{ eng: 'Manik ', hin: '', dir: '2', id: '6' },
+     //{ eng: 'Manik ', hin: '', dir: '2', id: '7' },
 
 ];
 
