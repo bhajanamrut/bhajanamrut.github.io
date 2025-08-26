0d49d76fc521eb0cbcd13759ae625d17e6299eae
shrinanabhajan <shrinanabhajan@gmail.com>
2025-08-26 22:49:27 +0530

    auto-index

--- scripts/part3.js
+++ scripts/part3.js
@@ -1,16 +1,53 @@
 var p3 = [
-    { eng: 'Manik Ganraj payi man jad', hin: 'गणराजपायीं मन जड जड', dir: '3', id: '1' },
-    { eng: 'Manik prabhuvin kon kunacha vali', hin: 'प्रभुविण कोण कुणाचा वाली', dir: '3', id: '2' },
-    { eng: 'Manik ig yenu pelali guraraya', hin: 'ईग येनु पेळलि गुरुराया', dir: '3', id: '3' },
-    { eng: 'Manik shri guru maza datta dayaghan', hin: 'श्री गुरु माझा दत्त दयाघन रे', dir: '3', id: '4' },
-    { eng: 'Manik guruji tore paiyape sees dharu', hin: 'गुरुजी तोरे पैयांपे सीस धरूं', dir: '3', id: '5' },
-    { eng: 'Manik dattasi gain pahin', hin: 'दत्तासी गाईन दत्तासी पाहीन', dir: '3', id: '6' },
-    { eng: 'Manik gurujin bolane khuntavile', hin: 'गुरुजीनें बोलणें खुंटविले', dir: '3', id: '7' },
-    { eng: 'Manik man laga mera avadhuta se', hin: 'मन लागा मेंरा अवधूता से', dir: '3', id: '8' },
-    //{ eng: 'Manik ', hin: '', dir: '3', id: '9' },
-    //{ eng: 'Manik ', hin: '', dir: '3', id: '10' },
-    //{ eng: 'Manik ', hin: '', dir: '3', id: '11' },
-    //{ eng: 'Manik ', hin: '', dir: '3', id: '12' },
+    {
+        eng: "Manik Ganraj payi man jad",
+        hin: "गणराजपायीं मन जड जड",
+        dir: "3",
+        id: "1"
+    },
+    {
+        eng: "Manik prabhuvin kon kunacha vali",
+        hin: "प्रभुविण कोण कुणाचा वाली",
+        dir: "3",
+        id: "2"
+    },
+    {
+        eng: "Manik ig yenu pelali guraraya",
+        hin: "ईग येनु पेळलि गुरुराया",
+        dir: "3",
+        id: "3"
+    },
+    {
+        eng: "Manik shri guru maza datta dayaghan",
+        hin: "श्री गुरु माझा दत्त दयाघन रे",
+        dir: "3",
+        id: "4"
+    },
+    {
+        eng: "Manik guruji tore paiyape sees dharu",
+        hin: "गुरुजी तोरे पैयांपे सीस धरूं",
+        dir: "3",
+        id: "5"
+    },
+    {
+        eng: "Manik dattasi gain pahin",
+        hin: "दत्तासी गाईन दत्तासी पाहीन",
+        dir: "3",
+        id: "6"
+    },
+    {
+        eng: "Manik gurujin bolane khuntavile",
+        hin: "गुरुजीनें बोलणें खुंटविले",
+        dir: "3",
+        id: "7"
+    },
+    {
+        eng: "Manik man laga mera avadhuta se",
+        hin: "मन लागा मेंरा अवधूता से",
+        dir: "3",
+        id: "8"
+    }
+
 ];
 
 if (typeof module === 'object' && module.exports) {
